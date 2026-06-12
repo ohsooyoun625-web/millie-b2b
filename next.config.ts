6ed11c06a31b5c2a3eae3275497cda12b2435e0d
Sue <sue@millie.town>
2026-06-12 10:49:43 +0900

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: "/millie-b2b",
+  assetPrefix: "/millie-b2b",
 };
 
 export default nextConfig;
